b9ff7443d43888b819c42a751c33cc12e0320b01
Rachell Ann Y. Pilo <rachellannypilo0824@gmail.com>
2026-04-28 22:04:49 +0800

    New File Created

---
+++ mls.py
@@ -0,0 +1,24 @@
+def initialize_library():
+
+    try:
+        file = open("MLS.txt", "x")
+        file.close()
+
+        print("Library file created successfully.")
+
+    except FileExistsError:
+        print("Library file already exists.")
+
+    file = open("MLS.txt", "w")
+
+    file.write("To Kill a Mockingbird\n")
+    file.write("Behind the Blue Sky\n")
+    file.write("Between the Rainbows\n")
+    file.write("Learn Computer Programming\n")
+
+    file.close()
+
+    print("Initial books added successfully.")
+
+
+initialize_library()
